7b0f136597c53bea35da0c14b8a0190e5958cc4b
John Garcia <johngarciadev@gmail.com>
2022-01-02 17:56:27 -0800

    Updated app.js with Game object initialization and event listener for game start button..

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+
+const game = new Game();
+const resetBtn = document.getElementById('btn__reset');
+const keyboard = document.getElementById('qwerty');
+
+
+/**
+ * EVENT LISTENERS
+ */
+
+// Game start
+resetBtn.addEventListener('click', ()=> {
+    game.startGame();
+});
