85c13bdbc1b079d2dce68da6c971ddd7df49efe6
John Garcia <johngarciadev@gmail.com>
2022-01-03 22:09:15 -0800

    Updated Game class with methods for handling game interaction and removing player lives.

--- js/Game.js
+++ js/Game.js
@@ -27,11 +27,60 @@ class Game {
     }
 
     /**
-     * Hides the start screen overlay, gets random phrase and sets activePhrase
+     * Sets up the game 
      */
     startGame() {
         document.getElementById('overlay').style.display = 'none';
         this.activePhrase = this.getRandomPhrase();
         this.activePhrase.addPhraseToDisplay();
     }
+
+    /**
+     * Checks to see if the button clicked by the player matches a letter in the phrase.
+     * Detects the selected latter's keyboard button then directs the game based on a correct or incorrect guess.
+     * @param {object} event captured by event listener
+     */
+    handleInteraction(event) {
+        
+        if (event.target.classList.contains('key')) {
+
+            const playerLetter = event.target.textContent;
+        
+            if (this.activePhrase.checkLetter(playerLetter)) {
+                this.activePhrase.showMatchedLetter(playerLetter);
+                event.target.classList.add('chosen');
+                // this.checkForWin();
+            } else {
+                event.target.classList.add('wrong');
+                this.removeLife();
+            }
+        }
+    }
+
+    /**
+     * Removes life from scoreboard
+     * Game ended once player has missed 5 times;
+     */
+    removeLife() {
+        if (this.missed < 5) {
+            const tries = document.getElementsByClassName('tries');
+            const currHeart = tries[this.missed].firstElementChild;
+            currHeart.setAttribute('src', 'images/lostHeart.png');
+            this.missed += 1;
+        } else {
+            // this.gameOver();
+        }
+    }
+
+    /**
+     * Checks to see if player has revealed all letters of the active phrase
+     */
+    checkForWin() {
+
+    }
+
+    /**
+     * Displays the original screen overlay and message based on game outcome
+     */
+    gameOver() {}
 }
